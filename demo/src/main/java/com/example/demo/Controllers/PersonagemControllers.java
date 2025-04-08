9f9b89365d0c91bb831b3da07fee449ac48be013
CaianL <160975630+CaianL@users.noreply.github.com>
2025-04-08 20:39:07 -0300

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Controllers/PersonagemControllers.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class PersonagemControllers {
+    @Autowired
+
+    @PostMapping("/addition"){
+
+    }
+    @GetMapping(""){
+
+    }
+
+    @PutMapping(""){
+
+    }
+
+    @DeleteMapping(""){
+
+    }
+
+}
